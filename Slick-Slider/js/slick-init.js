f5520f63c2a4a2406b74657d8c8acd9602bba79d
goodboyshelter <34268946+goodboyshelter@users.noreply.github.com>
2017-12-18 18:33:56 +0200

    Files

---
+++ Slick-Slider/js/slick-init.js
@@ -0,0 +1,53 @@
+
+    $('.slider').slick({
+    
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+                    },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+                    }
+                ]
+    });
+
+
+
+    $('.slider2').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [{
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+                    },
+            {
+                breakpoint: 576,
+                settings: {
+                    arrows: true,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+                    }
+                ]
+    });
